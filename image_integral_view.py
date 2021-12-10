7abf531e396f71c3e74d6f7c8b62e231457459a0
Nikita Tolstoy <nnnikitatolstoyyy@gmail.com>
2021-12-10 19:52:18 +0700

    integral_view fix

--- image_integral_view.py
+++ image_integral_view.py
@@ -1,9 +1,11 @@
+import copy
+
 
 def integral_view(image: list[list]) -> list[list]:
-    integral_matrix = list(image)
+    integral_matrix = copy.deepcopy(image)
 
-    for y in range(len(image)):
-        for x in range(len(image[0])):
+    for y in range(len(integral_matrix)):
+        for x in range(len(integral_matrix[0])):
             left_cell = integral_matrix[y][x - 1] if x > 0 else 0
             up_cell = integral_matrix[y - 1][x] if y > 0 else 0
             diagonal_cell = integral_matrix[y - 1][x - 1] if x > 0 and y > 0 else 0
